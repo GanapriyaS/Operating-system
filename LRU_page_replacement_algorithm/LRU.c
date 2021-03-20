798747e47298658efdf442f3fdeeebd0d6807dc3
GanapriyaS <67867470+GanapriyaS@users.noreply.github.com>
2021-03-21 01:41:12 +0530

    LRU

--- LRU_page_replacement_algorithm/LRU.c
+++ LRU_page_replacement_algorithm/LRU.c
@@ -1,1 +1,84 @@
+#include<stdio.h>
 
+int minLRU(int count[],int n_frames)
+{
+  int i,min=count[0],pos=0;
+  for(i=1;i<n_frames;i++)
+  {
+    if(count[i]<min)
+    {
+      min=count[i];
+      pos=i;
+    }
+  }
+  return pos;
+}
+
+int LRU(int pages[],int n_pages,int n_frames)
+{
+    int frames[n_frames],i,j,page_faults=0,count[n_frames],counter=0;
+    for(i=0;i<n_frames;i++)
+      frames[i]=-1;
+    for(int i=0;i<n_pages;i++)
+    {
+      int flag1=0,flag2=0;
+      for(int j=0;j<n_frames;j++)
+      {
+        if(pages[i]==frames[j])
+        {
+          count[j]=++counter;
+          flag1=flag2=1;
+          break;
+        }
+      }
+      if(flag1==0)
+      {
+        for(int j=0;j<n_frames;j++)
+        {
+          if(frames[j]==-1)
+          {
+            count[j]=++counter;
+            frames[j]= pages[i];
+            page_faults++;
+            flag2=1;
+            break;
+          }
+        }
+      }
+      if(flag2==0)
+      {
+        int pos= minLRU(count,n_frames);
+        count[pos]=++counter;
+        frames[pos]= pages[i];
+        page_faults++;
+      }
+      for(j=0;j<n_frames;j++)
+        printf("%d\t",frames[j]);
+      printf("\n");
+    }
+    return page_faults;
+}
+
+int main()
+{
+  int n_pages,n_frames,i;
+  printf("Enter the number of pages of process:");
+  scanf("%d",&n_pages);
+  printf("Enter the number of frames in main memory:");
+  scanf("%d",&n_frames);
+  printf("Enter the value of reference string:\n");
+  int pages[n_pages];
+  for(i=0;i<n_pages;i++)
+  {
+    printf("%d :",i+1);
+    scanf("%d",&pages[i]);
+  }
+  printf("Pages in the frames of main memory:\n");
+  int page_faults=LRU(pages,n_pages,n_frames);
+  printf("\nNumber of page_faults : %d",page_faults);
+  float missrate=(float)page_faults/n_pages;
+  printf("\nMiss Rate : %f",missrate);
+  printf("\nNumber of hits : %d",n_pages-page_faults);
+  printf("\nHit Rate : %f\n",1-missrate);
+  return 0;
+}
---
+++ LRU_page_replacement_algorithm/LRU.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<unordered_set>
+#include<unordered_map>
+using namespace std;
+
+int LRU(int pages[],int n_pages,int n_frames)
+{
+  unordered_set<int>task;
+  unordered_map<int,int>map;
+  int page_faults=0;
+  for(int i=0;i<n_pages;i++)
+  {
+    if(task.size() < n_frames)
+    {
+      if(task.find(pages[i])==task.end())
+      {
+        task.insert(pages[i]);
+        page_faults++;
+      }
+      map[pages[i]]=i;
+    }
+    else
+    {
+      if(task.find(pages[i])==task.end())
+      {
+        int temp,max=2147483647;
+        for(auto it=task.begin(); it!=task.end(); it++)
+        {
+          if(map[*it] < max)
+          {
+            max=map[*it];
+            temp=*it;
+          }
+        }
+        task.erase(temp);
+        task.insert(pages[i]);
+        page_faults++;
+      }
+      map[pages[i]]=i;
+    }
+  }
+  return page_faults;
+}
+
+
+
+int main()
+{
+  int n_pages,n_frames;
+  cout<<"Enter the number of pages of process:";
+  cin>>n_pages;
+  cout<<"Enter the number of frames in main memory:";
+  cin>>n_frames;
+  cout<<"Enter the value of reference string:\n";
+  int pages[n_pages];
+  for(int i=0;i<n_pages;i++)
+  {
+    cout<<i+1<<" :";
+    cin>>pages[i];
+  }
+  int page_faults=LRU(pages,n_pages,n_frames);
+  cout<<"Number of page_faults : "<<page_faults;
+  cout<<"\nMiss Rate : "<<(float)page_faults/n_pages;
+  cout<<"\nNumber of hits : "<<n_pages-page_faults;
+  cout<<"\nHit Rate : "<<1-((float)page_faults/n_pages);
+  return 0;
+}
