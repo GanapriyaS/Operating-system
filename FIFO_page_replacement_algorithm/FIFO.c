71306ae010cd018bff3a56df0de41cb0b14abb4a
GanapriyaS <67867470+GanapriyaS@users.noreply.github.com>
2021-03-21 01:38:46 +0530

    FIFO

--- FIFO_page_replacement_algorithm/FIFO.c
+++ FIFO_page_replacement_algorithm/FIFO.c
@@ -1,1 +1,54 @@
+#include<stdio.h>
 
+
+int FIFO(int pages[],int n_pages,int n_frames)
+{
+    int frames[n_frames],i,j,page_faults=0;
+    for(i=0;i<n_frames;i++)
+      frames[i]=-1;
+    for(int i=0;i<n_pages;i++)
+    {
+      int flag=0;
+      for(int j=0;j<n_frames;j++)
+      {
+        if(pages[i]==frames[j])
+        {
+          flag=1;
+          page_faults--;
+        }
+      }
+      page_faults++;
+      if((page_faults <= n_frames) && flag==0)
+        frames[i]= pages[i];
+      else if(flag==0)
+        frames[(page_faults-1)%n_frames]= pages[i];
+      for(j=0;j<n_frames;j++)
+        printf("%d\t",frames[j]);
+      printf("\n");
+    }
+    return page_faults;
+}
+
+int main()
+{
+  int n_pages,n_frames,i;
+  printf("Enter the number of pages of process:");
+  scanf("%d",&n_pages);
+  printf("Enter the number of frames in main memory:");
+  scanf("%d",&n_frames);
+  printf("Enter the value of reference string:\n");
+  int pages[n_pages];
+  for(i=0;i<n_pages;i++)
+  {
+    printf("%d :",i+1);
+    scanf("%d",&pages[i]);
+  }
+  printf("Pages in the frames of main memory:\n");
+  int page_faults=FIFO(pages,n_pages,n_frames);
+  printf("\nNumber of page_faults : %d",page_faults);
+  float missrate=(float)page_faults/n_pages;
+  printf("\nMiss Rate : %f",missrate);
+  printf("\nNumber of hits : %d",n_pages-page_faults);
+  printf("\nHit Rate : %f\n",1-missrate);
+  return 0;
+}
---
+++ FIFO_page_replacement_algorithm/FIFO.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<unordered_set>
+#include<queue>
+using namespace std;
+
+int FIFO(int pages[],int n_pages,int n_frames)
+{
+  unordered_set<int>task;
+  queue<int>q;
+  int page_faults=0;
+  for(int i=0;i<n_pages;i++)
+  {
+    if(task.size() < n_frames)
+    {
+      if(task.find(pages[i])==task.end())
+      {
+        task.insert(pages[i]);
+        page_faults++;
+        q.push(pages[i]);
+      }
+    }
+    else
+    {
+      if(task.find(pages[i])==task.end())
+      {
+        int temp=q.front();
+        q.pop();
+        task.erase(temp);
+        task.insert(pages[i]);
+        q.push(pages[i]);
+        page_faults++;
+      }
+    }
+  }
+  return page_faults;
+}
+
+
+
+int main()
+{
+  int n_pages,n_frames;
+  cout<<"Enter the number of pages of process:";
+  cin>>n_pages;
+  cout<<"Enter the number of frames in main memory:";
+  cin>>n_frames;
+  cout<<"Enter the value of reference string:\n";
+  int pages[n_pages];
+  for(int i=0;i<n_pages;i++)
+  {
+    cout<<i+1<<" :";
+    cin>>pages[i];
+  }
+  int page_faults=FIFO(pages,n_pages,n_frames);
+  cout<<"Number of page_faults : "<<page_faults;
+  cout<<"\nMiss Rate : "<<(float)page_faults/n_pages;
+  cout<<"\nNumber of hits : "<<n_pages-page_faults;
+  cout<<"\nHit Rate : "<<1-((float)page_faults/n_pages);
+  return 0;
+}
